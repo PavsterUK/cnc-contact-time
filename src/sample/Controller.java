028a2e2569580b2d7868303b7281d466d893afae
Pavel Naumovich <pavelnaumovic@gmail.com>
2020-11-10 22:07:21 +0000

    Managed to put together some sort of GUI, added IDs to buttons,textarea, textfield.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,22 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
+import javax.swing.*;
+
 public class Controller {
+
+    @FXML TextArea textArea;
+
+    public void ChooseFilePressed(ActionEvent actionEvent) {
+        JFileChooser chooser = new JFileChooser();
+        int returnVal = chooser.showOpenDialog(null);
+        if(returnVal == JFileChooser.APPROVE_OPTION) {
+            textArea.setText(chooser.getSelectedFile().getAbsolutePath());
+        }
+    }
 }
+
+
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,6 +1,7 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -8,12 +9,14 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("TOOL CONTACT TIME");
+        primaryStage.setScene(new Scene(root, 500, 800));
         primaryStage.show();
+        primaryStage.setResizable(false);
     }
 
 
